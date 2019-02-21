a19a8ad597588a2b128fa8906299f208fc9b4191
YuiTH <396698132@qq.com>
2019-02-21 16:59:46 +0800

    Revert "Init"
    This reverts commit 06b2fa26

--- src/app/shared/acc.ts
+++
@@ -1,9 +0,0 @@
-export class Acc {
-  x : number;
-  y : number;
-  z : number;
-  print(){
-    return "x:"+this.x+" y:"+this.y+" z:"+this.z;
-  }
-
-}
--- src/app/test.js
+++
@@ -1,5 +0,0 @@
-function f() {
-  var a = 10;
-}
-f();
-console.log(a);
