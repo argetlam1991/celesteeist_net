06b2fa260f55e2cad62254656793bd7aa033a842
YuiTH <396698132@qq.com>
2019-02-21 16:53:57 +0800

    Init

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -4,6 +4,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from '../service/authentication.service';
 import { ArticlesService } from '../service/articles.service';
 import { Article } from '../shared/article';
+import {Acc} from '../shared/acc';
+import {fromEvent, timer} from 'rxjs';
+import {debounce} from 'rxjs/operators';
 
 @Component({
   selector: 'app-article',
@@ -14,6 +17,9 @@ export class ArticleComponent implements OnInit {
 
   article: Article;
   is_admin = false;
+  is_mobile = false;
+  acc: Acc;
+  content : string;
 
   constructor(private auth: AuthenticationService,
               private articles: ArticlesService,
@@ -23,7 +29,38 @@ export class ArticleComponent implements OnInit {
   ngOnInit() {
     this.getArticle();
     this.checkIsAdmin();
+    this.AddListener()
   }
+  deviceMotionHandler(event){
+    let acc = new Acc();
+    acc.x = event.alpha;
+    acc.y = event.beta;
+    acc.z = event.gamma;
+    this.acc = acc;
+    console.log(acc);
+  }
+  AddListener(){
+    if (DeviceOrientationEvent) {
+      console.log("mobile!");
+      const source = fromEvent(window,'deviceorientation')
+        .pipe(debounce(()=>timer(500)));
+      const subscrible = source.subscribe(this.deviceMotionHandler);
+    // }
+    // if (DeviceOrientationEvent) {
+    //   console.log("Mobile!");
+    //   window.addEventListener('deviceorientation', this.deviceMotionHandler,false);
+    //   this.is_mobile = true;
+    //   if (this.article) {
+    //     if (!this.content) {
+    //       this.content = this.article.content;
+    //     }
+    //     this.article.content = this.content + this.acc.print();
+    //   }
+    }else {
+      console.log("not Mobile!");
+    }
+  }
+
 
   getArticle() {
     const article_id = this.route.snapshot.paramMap.get('article_id');
